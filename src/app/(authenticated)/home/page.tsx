2f4166992437385eff55caf69cc6d131eaf95d07
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-01-14 08:38:27 +0900

    implement api admin-statics

--- src/app/(authenticated)/home/page.tsx
+++ src/app/(authenticated)/home/page.tsx
@@ -2,9 +2,24 @@
 
 import React from "react";
 import { Grid, Card, CardContent, Typography } from "@mui/material";
-
+import {useQuery} from "@tanstack/react-query";
+import {Loading} from "@/components/Loading";
+import { Statics } from "@/app/api/admin/statics/route";
 
 const DashboardPage = () => {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ["admin-statics"],
+        queryFn: async () => {
+            const response = await fetch("/api/admin/statics")
+            if (!response.ok) throw new Error('Network response was not ok');
+            return response.json()
+        }
+    })
+
+    if (isLoading) return <Loading />
+    if (error) return <div>Error: {error.message}</div>;
+
+    const statics: Statics = data
 
     return (
         <Grid container spacing={3}>
@@ -12,14 +27,15 @@ const DashboardPage = () => {
                 <Card>
                     <CardContent>
                         <Typography variant="h5">Total Users</Typography>
-                        <Typography variant="h4">1,200</Typography>
+                        <Typography variant="h4">{statics.totalUsers}</Typography>
                     </CardContent>
                 </Card>
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12} md={4}>
                 <Card>
                     <CardContent>
-                        <Typography variant="h5">Sales Trend</Typography>
+                        <Typography variant="h5">DAU</Typography>
+                        <Typography variant="h4">{statics.dau}</Typography>
                     </CardContent>
                 </Card>
             </Grid>
---
+++ src/app/api/admin/statics/route.ts
@@ -0,0 +1,12 @@
+export type Statics = {
+    totalUsers: number;
+    dau: number;
+}
+
+export async function GET(): Promise<Response> {
+
+    return Response.json({
+        totalUsers: 1000,
+        dau: 100,
+    });
+}
