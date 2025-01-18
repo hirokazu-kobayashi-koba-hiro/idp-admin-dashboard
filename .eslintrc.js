8e87c7f57a3a5e42f80a4a6f6ba592f98c2dc2ef
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-01-18 09:21:58 +0900

    install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ["next", "next/core-web-vitals", "eslint:recommended"],
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:storybook/recommended"
+  ],
   rules: {
     "react/react-in-jsx-scope": "off", // Not needed in Next.js
     "jsx-a11y/anchor-is-valid": "off", // If using Link from Next.js
