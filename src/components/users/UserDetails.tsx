72b3be7c7a1e3b1d43174a20cd7821c4424d158f
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-01-21 08:59:30 +0900

    improve layout for user details

--- src/components/users/UserDetails.tsx
+++ src/components/users/UserDetails.tsx
@@ -1,22 +1,25 @@
-import {Box, TextField} from "@mui/material";
-import {User} from "@/app/api/admin/users/route";
+import { Grid } from "@mui/material";
+import { User } from "@/app/api/admin/users/route";
+import { LabeledTypography } from "@/components/LabeledTypography";
 
 type UserDetailsProps = {
-    user: User,
-}
+  user: User;
+};
 
 export const UserDetails = (props: UserDetailsProps) => {
-    const { user } = props;
+  const { user } = props;
 
-    return (
-        <Box sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            maxWidth: 400,
-            margin: "0 auto",
-        }}>
-            <TextField label={"id"} value={user.id} />
-        </Box>
-    )
-}
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={4}>
+        <LabeledTypography label={"id"} value={user.id} />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <LabeledTypography label={"name"} value={user.name} />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <LabeledTypography label={"email"} value={user.email} />
+      </Grid>
+    </Grid>
+  );
+};
