42b8daf72277a8d4823f55e259a536fbb93b6994
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-01-21 07:29:27 +0900

    implement page user detail

--- src/app/api/admin/users/[userId]/route.ts
+++ src/app/api/admin/users/[userId]/route.ts
@@ -1,7 +1,19 @@
 import { sleep } from "@/functions/sleep";
+import { userList } from "@/app/api/admin/users/route";
+import { NextRequest } from "next/server";
 
-export async function DELETE(userId: string): Promise<Response> {
-  console.log(userId);
+export async function GET(request: NextRequest, { params }: { params: { userId: string }}): Promise<Response> {
+  await sleep(500);
+  const user = userList.find((user) => user.id === params.userId);
+  console.log(params.userId, user);
+  if (!user) {
+    return Response.error();
+  }
+  return Response.json(user);
+}
+
+export async function DELETE(request: NextRequest, { params }: { params: { userId: string }}): Promise<Response> {
+  console.log(params.userId);
   await sleep(500);
   return Response.json(null);
 }
