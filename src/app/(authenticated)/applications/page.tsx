6cf7d95be2db14552dc870d31a1b90e1dc435a59
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-02-02 17:28:33 +0900

    add component to allow display and fix bug

---
+++ src/app/(authenticated)/applications/page.tsx
@@ -0,0 +1,12 @@
+import {Typography} from "@mui/material";
+
+const Applications = () => {
+
+    return (
+      <>
+      <Typography variant={"h4"}>Applications</Typography>
+      </>
+    );
+}
+
+export default Applications;
