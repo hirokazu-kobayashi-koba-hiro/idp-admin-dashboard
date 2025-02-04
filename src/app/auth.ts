de0ed3953f447e75a84a6cc2a497fcd2bd968f84
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-02-04 23:01:57 +0900

    implement provider for IdpServer

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,14 +1,107 @@
 import NextAuth from "next-auth";
 import Auth0Provider from "next-auth/providers/auth0";
+import {backendUrl} from "@/app/api/backendConfig";
+import {convertToCamel} from "@/functions/convertToCamel";
+
+const IdpServer = (options: any) => ({
+  ... {
+    id: "idp-server",
+    name: "IdPServer",
+    type: "oidc",
+    version: "2.0",
+    wellKnown: `${backendUrl}/.well-known/openid-configuration`,
+    idToken: false,
+    authorization: {
+      url: `${backendUrl}/v1/authorizations`,
+      params: {
+        scope: "openid profile phone email address",
+        client_id: process.env.IDP_ADMIN_DASHBOARD_CLIENT_ID,
+        response_type: "code",
+      }
+    },
+    checks: ["pkce", "state"],
+    token: {
+      async request(context: any) {
+        const { code } = context.params;
+        const params = new URLSearchParams({
+          grant_type: "authorization_code",
+          code,
+          redirect_uri: `http://localhost:3000/api/auth/callback/idp-server`,
+          client_id: process.env.IDP_ADMIN_DASHBOARD_CLIENT_ID as string
+        })
+        const response = await fetch(`${backendUrl}/api/v1/tokens`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          body: params,
+        })
+        if (!response.ok) {
+          console.log(response)
+          return {}
+        }
+
+        const body = await response.json()
+        console.log(body)
+
+        return {
+          ...body,
+        }
+      },
+    },
+    userinfo: {
+      async request(context: any) {
+        console.log(context.params);
+        const { access_token, refresh_token, expires_at } = context.params;
+
+        const response = await fetch(`${backendUrl}/api/v1/userinfo`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${access_token}`
+          },
+        })
+        if (!response.ok) {
+          console.log(response)
+          return {}
+        }
+
+        const body = await response.json()
+        console.log(body)
+
+        return {
+          access_token,
+          refresh_token,
+          expires_at,
+          ...body,
+        }
+      },
+    },
+    profile: (profile: any) => {
+      const converted = convertToCamel(profile)
+      return {
+        id: profile.sub,
+        ...converted
+      }
+    }
+  },
+  ...options,
+})
 
 export const { handlers } = NextAuth({
   providers: [
+    IdpServer({
+      clientId: process.env.IDP_ADMIN_DASHBOARD_CLIENT_ID,
+      clientSecret: process.env.IDP_ADMIN_DASHBOARD_CLIENT_SECRET,
+      issuer: process.env.IDP_SERVER_ISSUER,
+    }),
     Auth0Provider({
       clientId: process.env.NEXT_AUTH_AUTH0_CLIENT_ID,
       clientSecret: process.env.NEXT_AUTH_AUTH0_CLIENT_SECRET,
       issuer: process.env.NEXT_AUTH_AUTH0_ISSUER,
     }),
   ],
+  debug: true,
   callbacks: {
     async session({ session, trigger, newSession }) {
       // Note, that `rest.session` can be any arbitrary object, remember to validate it!
